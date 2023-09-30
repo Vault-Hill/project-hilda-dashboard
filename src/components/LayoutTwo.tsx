d5a034cb6f6e46bfec71f311c2551131a3727145
praise159praise <praisebamiyo159@gmail.com>
2023-09-30 12:32:41 +0000

    feat: added login page

--- src/components/LayoutTwo.tsx
+++ src/components/LayoutTwo.tsx
@@ -4,9 +4,12 @@ type Props = {
 
 const LayoutTwo: React.FC<Props> = ({ main }) => {
   return (
-    <>
-      <div>{main}</div>
-    </>
+    <div className="flex w-[100vw] h-[100vh]">
+      <div className="bg-black w-full flex justify-center items-center text-white text-[3rem] leading-[4rem] font-bold grid-repeating">
+        <div className="h-fit w-[40%] p-7 bg-[#ffffff18] rounded-2xl border-[#ffffff3d] border-[0.5px]">Avoiding Burnout to <br /> Build Performance</div>
+      </div>
+      <div className="w-full bg-[#D9D9D9] flex justify-center items-center">{main}</div>
+    </div>
   );
 };
 
